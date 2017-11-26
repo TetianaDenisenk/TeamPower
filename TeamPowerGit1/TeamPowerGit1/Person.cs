1bf0ef0b0a20d3f855e909d914b856c44a46871e
tetiana.kozlovska15 <tetiana.kozlovska15@gmail.com>
2017-11-26 21:24:42 +0200

    Revert "code refactoring"
    This reverts commit e14d9ab2f1cacd320d9d2186f23178c81adc3e89.

--- TeamPowerGit1/TeamPowerGit1/Person.cs
+++ TeamPowerGit1/TeamPowerGit1/Person.cs
@@ -1,6 +1,12 @@
-﻿namespace TeamPowerGit1
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamPowerGit1
 {
-    public  class Person
+  public  class Person
     {
         public string Name { get; set; }
         public string Address { get; set; }
