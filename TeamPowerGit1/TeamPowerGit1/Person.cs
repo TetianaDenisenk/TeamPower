e14d9ab2f1cacd320d9d2186f23178c81adc3e89
tetiana.kozlovska15 <tetiana.kozlovska15@gmail.com>
2017-11-26 21:22:10 +0200

    code refactoring

--- TeamPowerGit1/TeamPowerGit1/Person.cs
+++ TeamPowerGit1/TeamPowerGit1/Person.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TeamPowerGit1
+﻿namespace TeamPowerGit1
 {
-  public  class Person
+    public  class Person
     {
         public string Name { get; set; }
         public string Address { get; set; }
