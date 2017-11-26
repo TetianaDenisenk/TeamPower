6b886aa2fd0effe7c07ab332f4b16929dc485cb9
tetiana.kozlovska15 <tetiana.kozlovska15@gmail.com>
2017-11-26 23:15:47 +0200

    add weight

--- TeamPowerGit1/TeamPowerGit1/Person.cs
+++ TeamPowerGit1/TeamPowerGit1/Person.cs
@@ -9,6 +9,7 @@ namespace TeamPowerGit1
   public  class Person
     {
         public string Name { get; set; }
+        public int Weight { get; set; }
         public string Address { get; set; }
         public string LastName { get; set; }
     }
